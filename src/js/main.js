d056c4dbb2ccb6157a61c421a030351beaf11f68
Niclas Willems <niclas.willems@gmail.com>
2016-06-27 17:26:34 -0400

    Work progressing on import.  Need to finish select field and textarea.

--- src/js/main.js
+++ src/js/main.js
@@ -5,13 +5,31 @@ var baseURL = "http://json-data.herokuapp.com/forms";
 
 // The following function dynamically creates the form from the ajax call
 
+// WORKING FORM GENERATION
+// function form_template(field) {
+//     return `<div class="form_content"><form>
+//     <label for="${field.label}" class = "form_label">${field.label}</label>
+//     <input id="${field.id}" class = "form_field" type=${field.type}></br>
+//     <i class="fa ${field.icon}"></i></form>
+//   </div>`;
+// };
+
 function form_template(field) {
     return `<div class="form_content"><form>
     <label for="${field.label}" class = "form_label">${field.label}</label>
-    <input id="${field.id}" class = "form_field" type="text"></br>
+    <input id="${field.id}" class = "form_field" type=${field.type}></br>
     <i class="fa ${field.icon}"></i></form>
   </div>`;
 };
+
+// <select name="Select Language">
+//   <option value="EN">English</option>
+//   <option value="FR">French</option>
+//   <option value="SP">Spanish</option>
+//   <option value="CH">Chinese</option>
+//   <option value="JP">Japanese</option>
+// </select>
+
 
 // The following ajax call requests the form data, calls the form function
 // and appends the data to the node.
