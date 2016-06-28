a34697c45352db373eceeab963eda84d7657ef39
Niclas Willems <niclas.willems@gmail.com>
2016-06-27 22:48:44 -0400

    Select and textarea working. Last thing to try and figure out is removing the text labels when the underlying form fields get focus.

--- src/js/main.js
+++ src/js/main.js
@@ -3,41 +3,64 @@ import $ from 'jquery';
 var node = $(".form_body");
 var baseURL = "http://json-data.herokuapp.com/forms";
 
-// The following function dynamically creates the form from the ajax call
+// The following function dynamically creates for text, email, phone
 
-// WORKING FORM GENERATION
-// function form_template(field) {
-//     return `<div class="form_content"><form>
-//     <label for="${field.label}" class = "form_label">${field.label}</label>
-//     <input id="${field.id}" class = "form_field" type=${field.type}></br>
-//     <i class="fa ${field.icon}"></i></form>
-//   </div>`;
-// };
-
-function form_template(field) {
+function textTemplate(field) {
     return `<div class="form_content"><form>
-    <label for="${field.label}" class = "form_label">${field.label}</label>
-    <input id="${field.id}" class = "form_field" type=${field.type}></br>
-    <i class="fa ${field.icon}"></i></form>
+  <label for="${field.label}" class = "form_label">${field.label}</label>
+  <input id="${field.id}" class = "form_field" type=${field.type}></br>
+  <i class="fa ${field.icon}"></i></form>
   </div>`;
+}
+
+// The following function dynamically creates for textarea
+
+function textareaTemplate(field) {
+    return `<div class="form_textarea_field"><form>
+  <label for="${field.label}" class = "form_label">${field.label}</label>
+  <textarea name="${field.id}" rows="3" cols="50"></textarea></br>
+  <i class="fa ${field.icon}"></i></form>
+  </div>`;
+}
+
+// The following function dynamically creates for select
+
+function selectTemplate(field) {
+    var selectString = ""
+    var optionsString = ""
+    selectString = `<div class="form_select_field"><form><select name ="language_select">`
+    field.options.forEach(function(option) {
+        optionsString += `<option value = "${option.value}">${option.label}</option>`
+    })
+    return selectString += optionsString + `</select></form></div>`;
 };
 
-// <select name="Select Language">
-//   <option value="EN">English</option>
-//   <option value="FR">French</option>
-//   <option value="SP">Spanish</option>
-//   <option value="CH">Chinese</option>
-//   <option value="JP">Japanese</option>
-// </select>
+
+// The following function checks the field.type
+// and calls the appropriate function
+
+function generateHTML(field) {
+    if (field.type == "text") {
+        return textTemplate(field);
+    } else if (field.type == "textarea") {
+        return textareaTemplate(field);
+    } else if (field.type == "email") {
+        return textTemplate(field);
+    } else if (field.type == "tel") {
+        return textTemplate(field);
+    } else if (field.type == "select") {
+        return selectTemplate(field);
+    };
+};
 
 
 // The following ajax call requests the form data, calls the form function
 // and appends the data to the node.
 
 $.ajax({
-  url: baseURL,
-  dataType: "json"
-}).then(function(response){
-    var formHTML = response.map(form_template);
+    url: baseURL,
+    dataType: "json"
+}).then(function(response) {
+    var formHTML = response.map(generateHTML);
     node.append(formHTML);
 });
