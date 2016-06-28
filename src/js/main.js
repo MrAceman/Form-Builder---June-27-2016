36625707e387ee5d1a6992288c5dd35e878f8e46
Niclas Willems <niclas.willems@gmail.com>
2016-06-28 10:48:18 -0400

    Tweaks made during homework review

--- src/js/main.js
+++ src/js/main.js
@@ -4,22 +4,31 @@ var node = $(".form_body");
 var baseURL = "http://json-data.herokuapp.com/forms";
 
 // The following function dynamically creates for text, email, phone
+// WORKING CODE
+// function textTemplate(field) {
+//     return `<div class="form_content">
+//   <label for="${field.label}" class = "form_label">${field.label}</label>
+//   <input id="${field.id}" class = "form_field" type=${field.type}></br>
+//   <i class="fa ${field.icon}"></i>
+//   </div>`;
+// }
+
 
 function textTemplate(field) {
-    return `<div class="form_content"><form>
-  <label for="${field.label}" class = "form_label">${field.label}</label>
+    return `<div class="form_content" id="${field.id}">
+    <label for="${field.label}" class = "form_label">${field.label}</label>
   <input id="${field.id}" class = "form_field" type=${field.type}></br>
-  <i class="fa ${field.icon}"></i></form>
+  <i class="fa ${field.icon}"></i>
   </div>`;
 }
 
 // The following function dynamically creates for textarea
 
 function textareaTemplate(field) {
-    return `<div class="form_textarea_field"><form>
+    return `<div class="form_textarea_field">
   <label for="${field.label}" class = "form_label">${field.label}</label>
   <textarea name="${field.id}" rows="3" cols="50"></textarea></br>
-  <i class="fa ${field.icon}"></i></form>
+  <i class="fa ${field.icon}"></i>
   </div>`;
 }
 
@@ -28,11 +37,11 @@ function textareaTemplate(field) {
 function selectTemplate(field) {
     var selectString = ""
     var optionsString = ""
-    selectString = `<div class="form_select_field"><form><select name ="language_select">`
+    selectString = `<div class="form_select_field"><select name ="language_select">`
     field.options.forEach(function(option) {
         optionsString += `<option value = "${option.value}">${option.label}</option>`
     })
-    return selectString += optionsString + `</select></form></div>`;
+    return selectString += optionsString + `</select></div>`;
 };
 
 
@@ -40,15 +49,15 @@ function selectTemplate(field) {
 // and calls the appropriate function
 
 function generateHTML(field) {
-    if (field.type == "text") {
+    if (field.type === "text") {
         return textTemplate(field);
-    } else if (field.type == "textarea") {
+    } else if (field.type === "textarea") {
         return textareaTemplate(field);
-    } else if (field.type == "email") {
+    } else if (field.type === "email") {
         return textTemplate(field);
-    } else if (field.type == "tel") {
+    } else if (field.type === "tel") {
         return textTemplate(field);
-    } else if (field.type == "select") {
+    } else if (field.type === "select") {
         return selectTemplate(field);
     };
 };
